4b8f6d3625e5d9ac5b4d16698b3f6e9d9fe2a6c8
Samuel Le <samuelle107@gmail.com>
2020-09-12 11:56:40 -0500

    Require permission to warn

--- commands/warn.js
+++ commands/warn.js
@@ -5,16 +5,19 @@ module.exports = {
     description: 'Warn a user',
     execute(message, args) {
         const member = args[0];
+        const emoji = message.client.emojis.cache.get('753088260443734096');
         const msg = `아이고. ${member} has been warned!`;
         const msgGif = 'https://pa1.narvii.com/6390/23f1ef0fe7bf3f4c6de3c46cb6b1d8693089d5ad_hq.gif';
         const embed = new Discord.MessageEmbed()
             .setImage(msgGif)
             .setDescription(msg);
 
-        if (member) {
+        if (member && message.member.roles.cache.find(role => role.name === 'STAN RED RELVET')) {
             message.channel.send(embed);
+        } else if (!member && message.member.roles.cache.find(role => role.name === 'STAN RED RELVET')) {
+            message.channel.send(`아이고. You need to include the member ${emoji}`);
         } else {
-            message.channel.send('아이고. You need to include the member');
+            message.channel.send(`아이고. You do not have permission ${emoji}`);
         }
     }
 };
