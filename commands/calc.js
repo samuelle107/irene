6e78eae37d74f3998626fdddcdb23db5db0cb216
Samuel Le <samuelle107@gmail.com>
2020-09-12 23:45:30 -0500

    added calc command

---
+++ commands/calc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'calc',
+    description: 'Calucate an equation',
+    async execute(message, args) {
+        const equation = args.join('').replace(/\s/g, '');
+
+        try {
+            const result = eval(equation);
+            message.reply(`안녕! The result is **${result}**`);
+        } catch (error) {
+            message.reply('아이고! I cannot calculate that!');
+        }
+    }
+};
